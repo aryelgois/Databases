ea238df2518a9c2d1322e12eb6f8d41c05ec358f
Aryel Mota Góis <aryel.gois@gmail.com>
2017-12-25 22:51:06 -0300

    Update Address
    Almost a code rewrite
    
    - removed some sql comments
    - code improvement

--- src/Address.php
+++ src/Address.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * This Software is part of aryelgois\Databases and is provided "as is".
+ * This Software is part of aryelgois/Databases and is provided "as is".
  *
  * @see LICENSE
  */
 
 namespace aryelgois\Databases;
 
+use aryelgois\YaSql\Utils;
+use Symfony\Component\Yaml\Yaml;
+
 /**
- * Generates a .sql to populate Address database
- *
- * It uses a JSON as source
+ * Generates SQL to populate the Address database
  *
  * @author Aryel Mota Góis
  * @license MIT
@@ -19,11 +20,11 @@ namespace aryelgois\Databases;
 class Address
 {
     /**
-     * Path to JSONs
+     * Path to YAML sources
      *
      * @const string
      */
-    const PATH = __DIR__ . '/../data/Address';
+    const PATH = __DIR__ . '/../data/address';
 
     /**
      * How many entries can are allowed in each INSERT INTO statement
@@ -35,17 +36,22 @@ class Address
     /**
      * Added in the .sql header
      *
-     * @const string
+     * @const string[]
      */
-    const HEADER = "-- Address\n"
-                 . "--\n"
-                 . "-- A database with every Country, State and County.\n"
-                 . "-- Help to include more countries and to keep this database updated\n"
-                 . "--\n"
-                 . "-- Remember to run create.sql and populate_countries.sql before\n"
-                 . "--\n"
-                 . "-- @link https://www.github.com/aryelgois/Databases\n"
-                 . "USE address;\n";
+    const HEADER = [
+        '-- Address',
+        '--',
+        '-- A database with every Country, State and County.',
+        '--',
+        '-- Help to include more countries and to keep this database updated',
+        '--',
+        '-- Remember to build the database before',
+        '--',
+        '-- @link https://www.github.com/aryelgois/Databases',
+        '',
+        'USE address;',
+        '',
+    ];
 
     /**
      * Contains all loaded data from countries
@@ -55,21 +61,21 @@ class Address
     protected $countries;
 
     /**
-     * Creates a new Address object, loading countries data from JSONs
+     * Creates a new Address object, loading countries data from YAML files
      *
      * @param string[] $countries List of countries to be included
      *
-     * @throws RuntimeException If a country JSON is not found
+     * @throws \RuntimeException If a country YAML is not found
      */
     public function __construct(array $countries)
     {
         $data = [];
         foreach ($countries as $country_name) {
-            $file = realpath(self::PATH . '/source_' . $country_name . '.json');
+            $file = realpath(self::PATH . '/source_' . $country_name . '.yml');
             if (!file_exists($file)) {
                 throw new \RuntimeException('File not found');
             }
-            $data[$country_name] = json_decode(file_get_contents($file), true);
+            $data[$country_name] = Yaml::parse(file_get_contents($file));
         }
         $this->countries = $data;
     }
@@ -84,94 +90,78 @@ class Address
      */
     public function output($id_states = 0, $id_counties = 0)
     {
-        $sql = '';
+        $sql = self::HEADER;
         foreach ($this->countries as $country_name => $country) {
-            $chunk_states = [];
-            $count_states = 0;
-            $sql_states = $this->aboutCountry($country_name) . "\n";
-            $sql_counties = '';
-
+            $states = [];
+            $state_names = [];
+            $counties = [];
             foreach ($country['states'] as $state) {
-                if ($count_states == 0) {
-                    $sql_states .= "INSERT INTO `states` (`id`, `country`, `code`, `name`) VALUES\n";
-                }
-                $chunk_states[] = '(' . ++$id_states . ', ' . $country['country'] . ", '" . $state['code'] . "', '" . str_replace("'", "\\'", $state['name']) . "')";
-
-                $chunk_counties = [];
-                $count_counties = 0;
-                $sql_counties .= '-- ' . $country_name . ' -> ' . $state['name'] . "\n\n";
+                $states[] = sprintf(
+                    "(%s, %s, '%s', '%s')",
+                    ++$id_states,
+                    $country['country'],
+                    $state['code'],
+                    str_replace("'", "\\'", $state['name'])
+                );
                 foreach ($state['counties'] as $county) {
-                    if ($count_counties == 0) {
-                        $sql_counties .= "INSERT INTO `counties` (`id`, `state`, `name`) VALUES\n";
-                    }
-                    $chunk_counties[] = '(' . ++$id_counties . ', ' . $id_states . ", '" . str_replace("'", "\\'", $county) . "')";
-                    if (++$count_counties == self::CHUNK_SIZE) {
-                        $sql_counties .= implode(",\n", $chunk_counties) . ";\n\n";
-                        $chunk_counties = [];
-                        $count_counties = 0;
-                    }
+                    $counties[$id_states][] = sprintf(
+                        "(%s, %s, '%s')",
+                        ++$id_counties,
+                        $id_states,
+                        str_replace("'", "\\'", $county)
+                    );
                 }
-                if (!empty($chunk_counties)) {
-                    $sql_counties .= implode(",\n", $chunk_counties) . ";\n\n";
-                }
-
-
-                if (++$count_states == self::CHUNK_SIZE) {
-                    $sql_states .= implode(",\n", $chunk_states) . ";\n";
-                    $chunk_states = [];
-                    $count_states = 0;
-                }
-            }
-            if (!empty($chunk_states)) {
-                $sql_states .= implode(",\n", $chunk_states) . ";\n";
+                $state_names[$id_states] = $state['name'];
             }
 
-            $sql .= $sql_states . "\n"
-                  . $sql_counties . "\n";
+            $states = array_chunk($states, self::CHUNK_SIZE);
+            $states_sql = [];
+            foreach ($states as $chunk) {
+                $states_sql = array_merge(
+                    $states_sql,
+                    [
+                        '',
+                        'INSERT INTO `states` (`id`, `country`, `code`, `name`) VALUES',
+                    ],
+                    Utils::arrayAppendLast($chunk, ';', ',')
+                );
+            }
+
+            $counties_sql = [];
+            foreach ($counties as $id => $block) {
+                $counties_sql = array_merge(
+                    $counties_sql,
+                    [
+                        '',
+                        '-- ' . $state_names[$id],
+                    ]
+                );
+                $chunks = array_chunk($block, self::CHUNK_SIZE);
+                foreach ($chunks as $chunk) {
+                    $counties_sql = array_merge(
+                        $counties_sql,
+                        [
+                            '',
+                            'INSERT INTO `counties` (`id`, `state`, `name`) VALUES',
+                        ],
+                        Utils::arrayAppendLast($chunk, ';', ',')
+                    );
+                }
+            }
+
+            $count = count($country['states']);
+            $sql = array_merge(
+                $sql,
+                [
+                    '--',
+                    '-- ' . $country_name . ' (' . $count . ' State'. ($count > 1 ? 's' : '') . ')',
+                    '--',
+                ],
+                $states_sql,
+                $counties_sql
+            );
         }
 
-        $result = self::HEADER . "\n\n"
-                . $this->listCountries() . "\n\n"
-                . $sql
-                . "-- END";
-
-        return $result;
-    }
-
-    /**
-     * Generates SQL comment listing countries
-     *
-     * @return string
-     */
-    protected function listCountries()
-    {
-        $count = count($this->countries);
-        $result = '-- Populating ' . $count . ' Countr' . ($count > 1 ? 'ies' : 'y') . "\n"
-                . "--\n"
-                . "-- update     |  id | name\n"
-                . "-- ---------- | --- | ----\n";
-        foreach ($this->countries as $country_name => $country) {
-            $result .= '-- ' . $country['update'] . ' | ' . str_pad($country['country'], 3, ' ', STR_PAD_LEFT) . ' | ' . $country_name . "\n";
-        }
-        return $result;
-    }
-
-    /**
-     * Generates SQL comment about a country
-     *
-     * @param string $country_name The country which info will be generated
-     *
-     * @return string
-     */
-    protected function aboutCountry($country_name)
-    {
-        $country = $this->countries[$country_name];
-        $result = '-- ' . $country_name . "\n"
-                . "--\n"
-                . '-- ' . count($country['states']) . " States \n";
-        if (array_key_exists('notes', $country)) {
-            $result .= "--\n-- " . str_replace("\n", "\n-- ", $country['notes']) . "\n";
-        }
-        return $result;
+        return implode("\n", $sql);
     }
 }
